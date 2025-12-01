3b36f247e8e427f4dc046b7e16119e87ca0fcca7
joseph <josephcg.14@gmail.com>
2025-12-01 19:53:24 +0000

    chapter 1

---
+++ c1/vararith/fordegrees.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* print Farenheit-Celsius table
+* 	for fahr = 0, 20, ..., 300 */ 
+int main(){
+	int fahr;
+
+	for(fahr = 0; fahr <= 300; fahr = fahr + 20){
+		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+	}
+}
